c56af2e1cde6ab98d294f963bf538011c2ed6f44
Josh Humphries <jrdhumphries@gmail.com>
2015-02-10 21:36:23 +0000

    Use subscribe/publish in node

--- node/index.js
+++ node/index.js
@@ -4,52 +4,41 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var path = require('path');
 var redis = require("redis");
+var sender = redis.createClient();
+var receiver = redis.createClient();
 
 
 app.use(express.static(path.join(__dirname, 'static')));
 
-server.listen(3004);
-
-
-function addWorker(theKey, theReceiver) {
-    console.log("starting listening on key", theKey);
-    theReceiver.blpop(theKey, 0, function(err, data) {
-      console.log('processing data on', theKey, data[1]);
-      process.nextTick(function() {
-          addWorker(theKey, theReceiver);
-      });
-    });
-}
+server.listen(3000);
 
 
 io.on('connection', function (socket) {
-  var sender = redis.createClient();
-  var id = socket.conn.id;
-  var key = "ws." + id;
-  console.log("connection from", id, key);
+    var id = socket.conn.id;
+    var key = "ws." + id;
+    console.log("connection from:", id, "key: ", key);
+    receiver.subscribe(key);
 
-  addWorker(key, redis.createClient());
+    socket.on('some kind of message', function (data) {
+        console.log("Message from websocket:", data);
+        sender.publish(key, data)
+    });
 
-  socket.on('some kind of message', function (data) {
-    console.log("Message from websocket: '" + data + "'");
-    sender.rpush(key, data);
-    //receiver.subscribe('ws.' + id);
-    //sender.publish('ws.' + id, data)
-  });
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+        receiver.unsubscribe(channel);
+    });
 });
 
-//receiver.on("message", function (channel, message) {
-//    console.log("channel " + channel + ": " + message);
-//    var sock = io.sockets.connected[channel.substring(3)];
-//    if (sock) {
-//        sock.emit("banana", "woop!");
-//    } else {
-//        console.log('user is disconnected?');
-//        //shouldn't really be able to get here, but hey ho may as well make sure we clean up completely
-//        receiver.unsubscribe(channel);
-//    }
-//});
+
+receiver.on("message", function (channel, message) {
+    console.log("channel:", channel, "message:", message);
+    var sock = io.sockets.connected[channel.substring(3)];
+    if (sock) {
+        sock.emit("banana", "woop!");
+    } else {
+        console.log('user is disconnected?');
+        //shouldn't really be able to get here, but hey ho may as well make sure we clean up completely
+        receiver.unsubscribe(channel);
+    }
+});
