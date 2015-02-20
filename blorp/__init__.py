a0cbf8b8e574e65528b9d0c89059cb6e5dede181
Josh Humphries <jrdhumphries@gmail.com>
2015-02-20 21:43:50 +0000

    Some comments

--- blorp/__init__.py
+++ blorp/__init__.py
@@ -14,6 +14,13 @@ from blorp.responder import ResponderFactory, ResponderRouter
 
 
 def start(router_cls=ResponderRouter, factory_cls=ResponderFactory, event_loop=None):
+    """
+    Starts the websocket listening service with the given optional parameters. The event loop is started on the current
+    thread.
+    :param router_cls: the class to instantiate for the router object (optional)
+    :param factory_cls: the class to instantiate for the factory object (optional)
+    :param event_loop: the event loop to use (optional)
+    """
     factory = factory_cls()
     router = router_cls(factory, event_handlers)
     websocket_loop = ResponderLoop(router)
@@ -25,9 +32,17 @@ def start(router_cls=ResponderRouter, factory_cls=ResponderFactory, event_loop=N
 
 
 def start_in_new_thread(event_loop=None, **kwargs):
+    """
+    Starts the websocket listening service with the given optional parameters on a new thread and then returns that
+    thread and the event loop used as a tuple.
+    :param router_cls: the class to instantiate for the router object (optional)
+    :param factory_cls: the class to instantiate for the factory object (optional)
+    :param event_loop: the event loop to use (optional)
+    """
     if not event_loop:
+        # we need to make sure the event loop is created in this thread so that we can pass it back
         event_loop = asyncio.get_event_loop()
     kwargs.update({'event_loop': event_loop})
-    t = threading.Thread(target=start, kwargs=kwargs)
-    t.start()
-    return t, event_loop
+    thread = threading.Thread(target=start, kwargs=kwargs)
+    thread.start()
+    return thread, event_loop
