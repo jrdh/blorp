d237c3c455ebf2b54828f3fb8f737d12279416a8
Josh Humphries <jrdhumphries@gmail.com>
2015-03-08 16:16:13 +0000

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Blorp',
-    version='0.1.1',
+    version='0.2.0',
     author='Josh Humphries',
     author_email='jrdhumphries@gmail.com',
     packages=['blorp'],
